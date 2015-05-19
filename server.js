5cff53421412feb07a81bf6d3745775f8d7114d7
Rod Woodman <roderickwoodman@gmail.com>
2015-05-19 14:10:16 -0700

    added disconnect notification and scroll-to-bottom feature

--- server.js
+++ server.js
@@ -10,17 +10,24 @@ var server = app.listen(1232, function() {
 	console.log("listening to port 1232");
 });
 
+var users = {};
+
 var io = require('socket.io'). listen(server);
 io.sockets.on('connection', function (line) {
-	console.log("Initiating communication line...");
-	console.log("Line established, please stand by for instructions...");	
 	line.on("new_user", function(data) {
-		console.log("User "+data.name+" signed in.");
+		console.log(data.name+" SIGNED IN");
+		users[line.id] = data.name;
 		line.broadcast.emit("user_signed_in", {message:"User "+data.name+" signed in."});
 		io.emit("start_chat", {username: data.name});		
 	});
 	line.on("user_message", function(data) {
-		console.log("we are here "+ data.username+" "+data.message);
+		console.log(data.username+" POSTED '"+data.message+"'");
 		io.sockets.emit("message", {from: data.username, output: data.message});
+	});
+	line.on('disconnect', function () {
+		var leaving_user = users[line.id];
+		console.log(leaving_user+" LEFT CHAT");
+		line.broadcast.emit("user_left_chat", {message:"User "+leaving_user+" has left the chat."});
+		delete users[line.id];
 	});
 });
